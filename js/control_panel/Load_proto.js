9104ca8f0ec193868c9688315ba556701e4f508d
“peichunhuang-1” <p8l2n2s2g2k@gmail.com>
2024-02-06 17:30:47 +0800

    fix js proto path bugs

--- js/control_panel/Load_proto.js
+++ js/control_panel/Load_proto.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 function loadProtosFromFolder(folderPaths, protobuf) {
-  const proto_files = [];
+  const proto_files = {root: [], file: []};
   console.log(folderPaths);
   for (folderPath of folderPaths) {
   console.log(folderPath);
   const files = fs.readdirSync(folderPath);
   files.forEach((file) => {
       if (file.endsWith('.proto')) {
-        file = `${folderPath}/${file}`;
-        proto_files.push(file);
+        proto_files.root.push(folderPath);
+        proto_files.file.push(file);
       }
   });
   }
